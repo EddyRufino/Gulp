67a8f670cc9933b584089c5542d49238c4408a9b
eddy rufino <eddyjaair@gmail.com>
2019-01-20 20:13:14 -0500

    Primer Commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+
+
+gulp.task('sass', ()=> {
+    gulp.src('./dev/scss/styles.scss')
+        .pipe(sass({
+            outputStyle: 'compact'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions']
+        }))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(browserSync.stream())
+})
+
+gulp.task('pug', ()=> {
+    gulp.src('./dev/pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/'))
+})
+
+gulp.task('babel', ()=> {
+    gulp.src('./dev/js/scripts.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./public/js'))
+})
+
+gulp.task('default', ['sass'], ()=> {
+    browserSync.init({
+        server: './public'
+    })
+    gulp.watch('./dev/scss/styles.scss', ['sass'])
+    gulp.watch('./dev/pug/*.pug', ['pug']).on('change', browserSync.reload)
+    gulp.watch('./dev/js/*.js', ['babel']).on('change', browserSync.reload)
+})
